8263dc9c65e85bb6011d2e79c32dcf9360e15bee
caryz <Zhangkairui250@126.com>
2025-03-22 16:46:10 +0800

    JProfiler

---
+++ jprofiler11/doc/standalone/tree_data.js
@@ -0,0 +1,54 @@
+
+treeData = [
+  {label: 'Introduction', id: 'jprofiler.introduction', href: 'main/introduction.html'},
+  {label: 'Architecture', id: 'jprofiler.architecture', href: 'main/architecture.html'},
+  {label: 'Installing', id: 'jprofiler.installing', href: 'main/installing.html'},
+  {label: 'Profiling a JVM', id: 'jprofiler.profiling', href: 'main/profiling.html'},
+  {label: 'Recording data', id: 'jprofiler.recording', href: 'main/recording.html'},
+  {label: 'Snapshots', id: 'jprofiler.snapshots', href: 'main/snapshots.html'},
+  {label: 'Telemetries', id: 'jprofiler.telemetries', href: 'main/telemetries.html'},
+  {label: 'CPU profiling', id: 'jprofiler.cpu', href: 'main/cpu.html'},
+  {label: 'Memory profiling', id: 'jprofiler.memory', href: 'main/memory.html'},
+  {label: 'The heap walker', id: 'jprofiler.heapWalker', href: 'main/heapWalker.html'},
+  {label: 'Thread profiling', id: 'jprofiler.threads', href: 'main/threads.html'},
+  {label: 'Probes', id: 'jprofiler.probes', href: 'main/probes.html'},
+  {label: 'MBean browser', id: 'jprofiler.mbean', href: 'main/mbean.html'},
+  {label: 'Offline profiling', id: 'jprofiler.offline', href: 'main/offline.html'},
+  {label: 'Comparing snapshots', id: 'jprofiler.compare', href: 'main/compare.html'},
+  {label: 'IDE integrations', id: 'jprofiler.ide', href: 'main/ide.html'},
+  {label: 'Custom probes', id: 'jprofiler.customProbes.$folder$', href: 'customProbes/$folder$.html', children: [
+     {label: 'Probe concepts', id: 'jprofiler.customProbes.concepts', href: 'customProbes/concepts.html'},
+     {label: 'Script probes', id: 'jprofiler.customProbes.scriptProbes', href: 'customProbes/scriptProbes.html'},
+     {label: 'Injected probes', id: 'jprofiler.customProbes.injectedProbes', href: 'customProbes/injectedProbes.html'},
+     {label: 'Embedded probes', id: 'jprofiler.customProbes.embeddedProbes', href: 'customProbes/embeddedProbes.html'}
+  ]},
+  {label: 'Call tree features in detail', id: 'jprofiler.callTree.$folder$', href: 'callTree/$folder$.html', children: [
+     {label: 'Auto-tuning for instrumentation', id: 'jprofiler.callTree.autoTuning', href: 'callTree/autoTuning.html'},
+     {label: 'Async and remote request tracking', id: 'jprofiler.callTree.requestTracking', href: 'callTree/requestTracking.html'},
+     {label: 'Viewing parts of the call tree', id: 'jprofiler.callTree.rootsAndRemovals', href: 'callTree/rootsAndRemovals.html'},
+     {label: 'Splitting the call tree', id: 'jprofiler.callTree.splitting', href: 'callTree/splitting.html'},
+     {label: 'Call tree analyses', id: 'jprofiler.callTree.analyses', href: 'callTree/analyses.html'}
+  ]},
+  {label: 'Advanced CPU analysis views', id: 'jprofiler.advancedCpu.$folder$', href: 'advancedCpu/$folder$.html', children: [
+     {label: 'Method statistics', id: 'jprofiler.advancedCpu.methodStatistics', href: 'advancedCpu/methodStatistics.html'},
+     {label: 'Complexity analysis', id: 'jprofiler.advancedCpu.complexity', href: 'advancedCpu/complexity.html'},
+     {label: 'Call tracer', id: 'jprofiler.advancedCpu.callTracer', href: 'advancedCpu/callTracer.html'},
+     {label: 'Javascript XHR', id: 'jprofiler.advancedCpu.javaScriptXhr', href: 'advancedCpu/javaScriptXhr.html'}
+  ]},
+  {label: 'Heap walker features in detail', id: 'jprofiler.heapWalker.$folder$', href: 'heapWalker/$folder$.html', children: [
+     {label: 'HPROF snapshots', id: 'jprofiler.heapWalker.hprofSnapshots', href: 'heapWalker/hprofSnapshots.html'},
+     {label: 'Minimizing overhead', id: 'jprofiler.heapWalker.overhead', href: 'heapWalker/overhead.html'},
+     {label: 'Filters and live interactions', id: 'jprofiler.heapWalker.referenceFiltering', href: 'heapWalker/referenceFiltering.html'},
+     {label: 'Finding memory leaks', id: 'jprofiler.heapWalker.memoryLeaks', href: 'heapWalker/memoryLeaks.html'}
+  ]},
+  {label: 'Configuration in detail', id: 'jprofiler.advancedConfig.$folder$', href: 'advancedConfig/$folder$.html', children: [
+     {label: 'Trouble shooting connection problems', id: 'jprofiler.advancedConfig.troubleShooting', href: 'advancedConfig/troubleShooting.html'},
+     {label: 'Scripts', id: 'jprofiler.advancedConfig.scripts', href: 'advancedConfig/scripts.html'}
+  ]},
+  {label: 'Command line reference', id: 'jprofiler.commandLine.$folder$', href: 'commandLine/$folder$.html', children: [
+     {label: 'Executables for profiling', id: 'jprofiler.commandLine.profilingExecutables', href: 'commandLine/profilingExecutables.html'},
+     {label: 'Executables for snapshots', id: 'jprofiler.commandLine.snapshotExecutables', href: 'commandLine/snapshotExecutables.html'},
+     {label: 'Gradle tasks', id: 'jprofiler.commandLine.gradleTasks', href: 'commandLine/gradleTasks.html'},
+     {label: 'Ant tasks', id: 'jprofiler.commandLine.antTasks', href: 'commandLine/antTasks.html'}
+  ]}
+];
