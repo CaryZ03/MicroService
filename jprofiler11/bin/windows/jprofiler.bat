d0cae2c0dd2e8b5e2d406f14b5c752fab221225c
caryz <Zhangkairui250@126.com>
2025-04-03 16:30:30 +0800

    1

--- jprofiler11/bin/windows/jprofiler.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOCALCLASSPATH=..\jprofiler.jar;..\..\.install4j\i4jruntime.jar
-for %%i in (..\..\lib\*.jar) do call lcp.bat %%i
-for %%i in (..\..\lib\probe\*.jar) do call lcp.bat %%i
-
-java -Xmx512m -classpath "%LOCALCLASSPATH%" com.jprofiler.frontend.FrontendApplication
